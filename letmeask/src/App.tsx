546e86bc873856cc09c36870a59dc4f8eb6ec151
Carlos <carlosoftwar@gmail.com>
2021-06-22 12:17:24 -0300

    feat: add user auth with google

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,12 +1,19 @@
-import Button from './components/button'
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './styles/global.scss';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
+
   return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-    </>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact={true} component={Home}></Route>
+        <Route path="/rooms/new" component={NewRoom}></Route>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
